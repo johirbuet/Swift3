8e0f01c5613012ec82799d520b49d398d6273915
Johirul Islam <johirbuet@gmail.com>
2017-05-12 14:46:40 -0500

    Chapter 2

--- Ch2.playground/Contents.swift
+++ Ch2.playground/Contents.swift
@@ -19,3 +19,20 @@ var logic = "!, &&, ||"
 var closedRange = "value1...value2"
 
 var halfOpenRange = "value1..<value2"
+
+// Declaring variables and types
+
+var canChange = "This variable can change"
+let cannotChange = "This variable cant change"
+
+canChange = "Here's proof"
+//cannotChange = "This will give error"
+
+// Explicitly typed variable
+
+var playerHealth_Explitcit : Int = 100
+
+var playerHealth_inferred = 100
+
+// Multiple Explicit typed variable
+var playerDamage: Int, playerAttack: Int, playerGold: Double
