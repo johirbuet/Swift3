e0b9ecf256f6133530ced7116c68aa8eb9a41fe0
Johirul Islam <johirbuet@gmail.com>
2017-05-12 14:40:30 -0500

    some operators

---
+++ Ch2.playground/Contents.swift
@@ -0,0 +1,21 @@
+/*
+ Chapter 2 Back to Variables and operators
+ */
+
+import UIKit
+
+var assignMentOperator = "="
+
+var mathOperators = "=, -, *, /"
+
+var remainderOperators = "%"
+
+var compoundOperators = "+=, -= *="
+
+var comparisonOperators = "==, !=, >, <,<=, >="
+
+var logic = "!, &&, ||"
+
+var closedRange = "value1...value2"
+
+var halfOpenRange = "value1..<value2"
